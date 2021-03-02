4fda160ecff059ecdd07465a5191f111267118a0
павел чукланов <chuklanov.pavel@gmail.com>
2021-03-02 23:39:51 +0300

    zagotovka

--- NoteMathBot/datd.py
+++ NoteMathBot/datd.py
@@ -3,6 +3,36 @@ import json
 def add_image(id):
     with open("dat.json", "r") as read_file:
         dat = json.load(read_file)
-    dat
+    dat[str(id)] = [str(id)+'.jpg']
     with open("dat.json", "w") as write_file:
         json.dump(dat, write_file)
+
+def add_text(id, text):
+    with open("dat.json", "r") as read_file:
+        dat = json.load(read_file)
+
+    t = text.split('&')
+    print(text)
+    print(t)
+    bil= t[2].split()
+    dat[str(id)].append(t[0]) #первый текст
+    dat[str(id)].append(t[1]) #второй текст
+    dat[str(id)].append(bil) #использованные материалы
+    dat[str(id)].append(t[3]) #имя теоремы
+    with open("dat.json", "w") as write_file:
+        json.dump(dat, write_file)
+
+def get(id):
+    with open("dat.json", "r") as read_file:
+        dat = json.load(read_file)
+    otv = ''
+
+    for i in dat[id][3]:
+        ia = int(i)
+        with open("dat.json", "r") as read_file:
+            v = json.load(read_file)
+
+        #print(v[i])
+        otv += ("{0}: /{1}, ".format(v[i][4],i))
+    dat[str(id)].append(otv)
+    return dat[str(id)]
--- NoteMathBot/main.py
+++ NoteMathBot/main.py
@@ -1,6 +1,12 @@
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton,InlineKeyboardMarkup
+import datd
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+
 import json
+import os
+
 
 #подгружаем JSON с юзерами
 users = {}
@@ -19,25 +25,28 @@ config_txt.close()
 bot = Bot(token=(config[0]))
 dp = Dispatcher(bot)
 
+delay = 0
 ###############
 #сам ботяра
 @dp.message_handler(commands=['start'])
 async def echo(message: types.Message):
-    # m11 = InlineKeyboardButton("алгебра", callback_data='bt2')
-    # m12 = InlineKeyboardButton("геометрия 😎", callback_data='bt1')
-    # greet_kb = InlineKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(m11, m12)
+    b_fiz = KeyboardButton('математика 💣')
+    b_math = KeyboardButton('физика 👋')
+    greet_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    greet_kb.row(b_fiz,b_math)
     if str(message.from_user.id) not in users:
         users[str(message.from_user.id)]= {"status":0}
         with open("data.json", "w") as write_file:
             json.dump(users, write_file)
-        await message.answer("тут можно посмотреть все билеты по геометрии и другии теоремы \n \n выбери режим:")
+        await message.answer("тут можно посмотреть все билеты по геометрии и другии теоремы \n \n выбери режим:", reply_markup=greet_kb)
     else:
-        await message.answer("выбери режим:")
+        await message.answer("выбери режим:", reply_markup=greet_kb)
     print(users)
 
 @dp.message_handler(content_types=['photo'])
 async def photoes(message):
     global config
+    global delay
 
     new = str(int(config[1])+1)
     await message.photo[-1].download(new+'.jpg')
@@ -53,8 +62,44 @@ async def photoes(message):
     print(cn)
     config_txt.seek(0)
     config_txt.write(cn)
-
+    datd.add_image(new)
     config_txt.close()
+    delay = new
+
+@dp.message_handler(content_types=['text'])
+async def maint(message : types.Message):
+    global delay
+    if delay != 0:
+        datd.add_text(delay, message.text)
+        delay = 0
+        await bot.send_message(message.from_user.id, 'Приnal')
+    else:
+        if message.text[0] == '/':
+            num = message.text[1:]
+            inf = datd.get(num)
+            await bot.send_message(message.from_user.id, inf[1])
+            f = open(inf[0], "rb")
+            await Bot.send_photo(self=bot, chat_id=message.from_user.id, photo=f)
+            f.close()
+            await bot.send_message(message.from_user.id, inf[2])
+            markdown = """
+                            *bold text*
+                            _italic text_
+                            [text](URL)
+                            """
+
+            await bot.send_message(message.from_user.id, "использованные материалы\n"+inf[5])
+        elif message.text == 'физика 👋':
+            await message.answer("патом")
+        elif message.text == 'математика 💣':
+
+            b_fiz = KeyboardButton('билеты 🤩')
+            b_math = KeyboardButton('новые теоремы')
+            greet_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+            greet_kb.row(b_fiz, b_math)
+            await message.answer(reply_markup=greet_kb)
+
+
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)  # запуск
